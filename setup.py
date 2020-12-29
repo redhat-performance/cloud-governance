dd8509a48a81689ff1b7539af973c4693c284e8c
ebattat <ebattat@red-hat.com>
2020-12-29 11:29:00 +0200

    fix environ main

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from codecs import open
 from os import path
 from setuptools import setup, find_packages
 
-__version__ = '1.0.6'
+__version__ = '1.0.7'
 here = path.abspath(path.dirname(__file__))
 
 
