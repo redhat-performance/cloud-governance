5dc078f82485b045a8c4aab6f177bd6f93f1eb01
ebattat <ebattat@red-hat.com>
2020-12-29 14:14:27 +0200

    add policies support in docker

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.8-slim
 
 RUN python -m pip install --upgrade pip && pip install cloud-governance
 RUN pip3 --no-cache-dir install --upgrade awscli
-ADD policies /usr/local/
-COPY cloud_governance/main/run.py /usr/local/run.py
+ADD cloud_governance/policies /usr/local/cloud_governance/policies/
+COPY cloud_governance/main/run.py /usr/local/cloud_governance/run.py
 
-CMD [ "python", "/usr/local/run.py"]
+CMD [ "python", "/usr/local/cloud_governance/run.py"]
---
+++ cloud_governance/main/main.py
@@ -0,0 +1,56 @@
+import os
+from time import gmtime, strftime  # date tag
+from ast import literal_eval  # str to dict
+from os import listdir
+from os.path import isfile, join  # list files
+
+from cloud_governance.tag_cluster.run_tag_cluster_resouces import scan_cluster_resource, tag_cluster_resource
+from cloud_governance.zombie_cluster.run_zombie_cluster_resources import zombie_cluster_resource, delete_zombie_cluster_resource
+
+# env tests
+# os.environ['AWS_DEFAULT_REGION'] = 'us-east-2'
+# os.environ['action'] = 'policies'
+# os.environ['policies_output'] ='s3://redhat-custodian/logs'
+# os.environ['dry_run'] = '--dryrun'
+# os.environ['policy'] = 'ebs_unattached.yml'
+#os.environ['action'] = 'tag_cluster_resource'
+#os.environ['cluster_name'] = 'ocs-test'
+#os.environ['mandatory_tags'] = "{'Owner': 'Eli Battat','Email': 'ebattat@redhat.com','Purpose': 'test'}"
+
+# environment variables - get while running the docker
+region = os.environ['AWS_DEFAULT_REGION']
+action = os.environ['action']
+
+
+def main():
+    """
+    This is the main for running actions
+    :return: the action output
+    """
+    if action == 'policies':
+        dry_run = os.environ['dry_run']
+        policy = os.environ['policy']
+        policies_output = os.environ['policies_output']
+        policies_path = os.path.join(os.path.dirname(__file__), f'../{action}')
+        if policy:
+            os.system(f'custodian run {dry_run} -s {policies_output} {policies_path}/{policy}')
+        else:  # run all policies
+            policy_files = [f for f in listdir(policies_path) if isfile(join(policies_path, f))]
+            for policy in policy_files:
+                os.system(f'custodian run {dry_run} -s {policies_output} {policies_path}/{policy}')
+    elif action == 'scan_cluster_resource':
+        cluster_name = os.environ['cluster_name']
+        scan_cluster_resource(cluster_name=cluster_name, region=region)
+    elif action == 'tag_cluster_resource':
+        cluster_name = os.environ['cluster_name']
+        mandatory_tags = os.environ['mandatory_tags']
+        mandatory_tags = literal_eval(mandatory_tags)
+        mandatory_tags['Date'] = strftime("%Y/%m/%d %H:%M:%S")
+        tag_cluster_resource(cluster_name=cluster_name, mandatory_tags=mandatory_tags, region=region)
+    elif action == 'zombie_cluster_resource':
+        zombie_cluster_resource(delete=False, region=region)
+    elif action == 'delete_zombie_cluster_resource':
+        delete_zombie_cluster_resource(delete=True, region=region)
+
+
+main()
