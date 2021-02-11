bfff9408efbc9f6ee8fe38abed2eeb496461a980
ebattat <ebattat@redhat.com>
2021-02-11 16:17:25 +0200

    fix upload pass parameters1

--- cloud_governance/main/job.sh
+++ cloud_governance/main/job.sh
@@ -12,24 +12,28 @@ do
 done
 
 # 2. Upload data to ElasticSearch - ec2 index
+es_host='localhost'
+es_index='json_ec2_timestamp_index'
 declare -a regions=('us-east-1' 'us-east-2' 'us-west-1' 'us-west-2')
 declare -a policies=('ec2_idle' 'ec2_untag')
 for region in "${regions[@]}"
 do
    for policy in "${policies[@]}"
    do
-       sudo podman run --rm --name cloud-governance -e upload_data_es='upload_data_es' -e es_host='elasticsearch.intlab.perf-infra.lab.eng.rdu2.redhat.com' -e es_port='9200' -e es_index='json_ec2_timestamp_index' -e bucket=$bucket -e policy=$policy -e region=$region -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e log_level=INFO quay.io/ebattat/cloud-governance
+       sudo podman run --rm --name cloud-governance -e upload_data_es='upload_data_es' -e es_host=$es_host -e es_port='9200' -e es_index=$es_index -e bucket=$bucket -e policy=$policy -e region=$region -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e log_level=INFO quay.io/ebattat/cloud-governance
 
    done
 done
 
 # 3. Upload data to ElasticSearch - ebs index
+es_host='localhost'
+es_index='json_ebs_timestamp_index'
 declare -a regions=('us-east-1' 'us-east-2' 'us-west-1' 'us-west-2')
 declare -a policies=('ebs_unattached')
 for region in "${regions[@]}"
 do
    for policy in "${policies[@]}"
    do
-       sudo podman run --rm --name cloud-governance -e upload_data_es='upload_data_es' -e es_host='elasticsearch.intlab.perf-infra.lab.eng.rdu2.redhat.com' -e es_port='9200' -e es_index='json_ebs_timestamp_index' -e bucket=$bucket -e policy=$policy -e region=$region -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e log_level=INFO quay.io/ebattat/cloud-governance
+       sudo podman run --rm --name cloud-governance -e upload_data_es='upload_data_es' -e es_host=$es_host -e es_port='9200' -e es_index=$es_index -e bucket=$bucket -e policy=$policy -e region=$region -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e log_level=INFO quay.io/ebattat/cloud-governance
    done
 done
