35d398478d5d5fe5baa582225de34c56822eb3a6
ebattat <ebattat@redhat.com>
2021-02-14 15:44:07 +0200

    split job to 2 steps run policies and upload es

---
+++ cloud_governance/main/run_policies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+AWS_ACCESS_KEY_ID=$1
+AWS_SECRET_ACCESS_KEY=$2
+BUCKET=$3
+
+echo "Run all policies pre active region"
+declare -a regions=('us-east-1' 'us-east-2' 'us-west-1' 'us-west-2')
+declare -a policies=('ec2_idle' 'ebs_unattached' 'ec2_untag')
+
+for region in "${regions[@]}"
+do
+   for policy in "${policies[@]}"
+   do
+       sudo podman run --rm --name cloud-governance -e policy=$policy -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$region -e dry_run=yes -e policy_output=s3://$BUCKET/logs/$region -e log_level=INFO quay.io/ebattat/cloud-governance
+   done
+done
