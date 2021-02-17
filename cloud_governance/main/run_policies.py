6a6a1dec8bf21a287157374bd9b73c9af65b76aa
ebattat <ebattat@redhat.com>
2021-02-18 01:22:11 +0200

    move shell to python

--- cloud_governance/main/run_policies.py
+++ cloud_governance/main/run_policies.py
@@ -11,8 +11,8 @@ regions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2']
 policies = ['ec2_idle', 'ec2_untag', 'ec2_run', 'ebs_unattached']
 
 for region in regions:
-   for policy in policies:
-       os.system(f"sudo podman run --rm --name cloud-governance -e policy={policy} -e AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY} -e AWS_DEFAULT_REGION={region} -e dry_run=yes -e policy_output=s3://{BUCKET}/logs/{region} -e log_level=INFO quay.io/ebattat/cloud-governance")
+    for policy in policies:
+        os.system(f"sudo podman run --rm --name cloud-governance -e policy={policy} -e AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY} -e AWS_DEFAULT_REGION={region} -e dry_run=yes -e policy_output=s3://{BUCKET}/logs/{region} -e log_level=INFO quay.io/ebattat/cloud-governance")
 
 
 print("run gitleaks")
