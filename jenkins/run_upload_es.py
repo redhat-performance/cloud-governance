d6866d435b5e7f656709e81b2e832ddfe90bbbd7
ebattat <ebattat@redhat.com>
2021-02-20 21:11:28 +0200

    add account to upload es

--- jenkins/run_upload_es.py
+++ jenkins/run_upload_es.py
@@ -34,8 +34,8 @@ policies = get_custodian_policies(type='ec2')
 es_index = 'cloud-governance-ec2'
 for region in regions:
     for policy in policies:
-        os.system(f"sudo podman run --rm --name cloud-governance -e upload_data_es='upload_data_es' -e es_host={ES_HOST} -e es_port={ES_PORT} -e es_index={es_index} -e bucket={BUCKET_PERF} -e policy={policy} -e AWS_DEFAULT_REGION={region} -e AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID_PERF} -e AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY_PERF} -e log_level=INFO quay.io/ebattat/cloud-governance")
-        #os.system(f"sudo podman run --rm --name cloud-governance -e upload_data_es='upload_data_es' -e es_host={ES_HOST} -e es_port={ES_PORT} -e es_index={es_index} -e bucket={BUCKET_PSAP} -e policy={policy} -e AWS_DEFAULT_REGION={region} -e AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID_PSAP} -e AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY_PSAP} -e log_level=INFO quay.io/ebattat/cloud-governance")
+        os.system(f"sudo podman run --rm --name cloud-governance -e upload_data_es='upload_data_es' -e account='perf' -e es_host={ES_HOST} -e es_port={ES_PORT} -e es_index={es_index} -e bucket={BUCKET_PERF} -e policy={policy} -e AWS_DEFAULT_REGION={region} -e AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID_PERF} -e AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY_PERF} -e log_level=INFO quay.io/ebattat/cloud-governance")
+        #os.system(f"sudo podman run --rm --name cloud-governance -e upload_data_es='upload_data_es' -e account='psap' -e es_host={ES_HOST} -e es_port={ES_PORT} -e es_index={es_index} -e bucket={BUCKET_PSAP} -e policy={policy} -e AWS_DEFAULT_REGION={region} -e AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID_PSAP} -e AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY_PSAP} -e log_level=INFO quay.io/ebattat/cloud-governance")
 
 
 print("Upload data to ElasticSearch - ebs index")
@@ -43,8 +43,8 @@ es_index = 'cloud-governance-ebs'
 policies = get_custodian_policies(type='ebs')
 for region in regions:
     for policy in policies:
-        os.system(f"sudo podman run --rm --name cloud-governance -e upload_data_es='upload_data_es' -e es_host={ES_HOST} -e es_port={ES_PORT} -e es_index={es_index} -e bucket={BUCKET_PERF} -e policy={policy} -e AWS_DEFAULT_REGION={region} -e AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID_PERF} -e AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY_PERF} -e log_level=INFO quay.io/ebattat/cloud-governance")
-        #os.system(f"sudo podman run --rm --name cloud-governance -e upload_data_es='upload_data_es' -e es_host={ES_HOST} -e es_port={ES_PORT} -e es_index={es_index} -e bucket={BUCKET_PSAP} -e policy={policy} -e AWS_DEFAULT_REGION={region} -e AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID_PSAP} -e AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY_PSAP} -e log_level=INFO quay.io/ebattat/cloud-governance")
+        os.system(f"sudo podman run --rm --name cloud-governance -e upload_data_es='upload_data_es' -e account='perf' -e es_host={ES_HOST} -e es_port={ES_PORT} -e es_index={es_index} -e bucket={BUCKET_PERF} -e policy={policy} -e AWS_DEFAULT_REGION={region} -e AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID_PERF} -e AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY_PERF} -e log_level=INFO quay.io/ebattat/cloud-governance")
+        #os.system(f"sudo podman run --rm --name cloud-governance -e upload_data_es='upload_data_es' -e account='psap' -e es_host={ES_HOST} -e es_port={ES_PORT} -e es_index={es_index} -e bucket={BUCKET_PSAP} -e policy={policy} -e AWS_DEFAULT_REGION={region} -e AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID_PSAP} -e AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY_PSAP} -e log_level=INFO quay.io/ebattat/cloud-governance")
 
 
 # Gitleaks run on github not related to any aws account
@@ -52,4 +52,4 @@ print("Upload data to ElasticSearch - gitleaks index")
 es_index = 'cloud-governance-gitleaks'
 region = 'us-east-1'
 policy = 'gitleaks'
-os.system(f"sudo podman run --rm --name cloud-governance -e upload_data_es='upload_data_es' -e es_host={ES_HOST} -e es_port={ES_PORT} -e es_index={es_index} -e bucket={BUCKET_PERF} -e policy={policy} -e AWS_DEFAULT_REGION={region} -e AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID_PERF} -e AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY_PERF} -e log_level=INFO quay.io/ebattat/cloud-governance")
+os.system(f"sudo podman run --rm --name cloud-governance -e upload_data_es='upload_data_es' -e account='perf' -e es_host={ES_HOST} -e es_port={ES_PORT} -e es_index={es_index} -e bucket={BUCKET_PERF} -e policy={policy} -e AWS_DEFAULT_REGION={region} -e AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID_PERF} -e AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY_PERF} -e log_level=INFO quay.io/ebattat/cloud-governance")
