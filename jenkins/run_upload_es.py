c4a86d27c192e75cc969a6e0979586a0617986a4
ebattat <ebattat@redhat.com>
2021-02-24 14:58:56 +0200

    change ec2 index

--- jenkins/run_upload_es.py
+++ jenkins/run_upload_es.py
@@ -31,7 +31,7 @@ regions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2']
 
 print("Upload data to ElasticSearch - ec2 index")
 policies = get_custodian_policies(type='ec2')
-es_index = 'cloud-governance-ec2'
+es_index = 'cloud-governance-ec2-index'
 for region in regions:
     for policy in policies:
         os.system(f"sudo podman run --rm --name cloud-governance -e upload_data_es='upload_data_es' -e account='perf' -e es_host={ES_HOST} -e es_port={ES_PORT} -e es_index={es_index} -e bucket={BUCKET_PERF} -e policy={policy} -e AWS_DEFAULT_REGION={region} -e AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID_PERF} -e AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY_PERF} -e log_level=INFO quay.io/ebattat/cloud-governance")
