b08f90229bfa9bccf5c9aab66d18006f20803267
AARAVETI THIRUMALESH <athiruma@athiruma.remote.csb>
2022-01-20 15:35:27 +0530

    Updated python versions in setup.py

--- setup.py
+++ setup.py
@@ -21,13 +21,15 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Red Hat',
-    author_email='ebattat@redhat.com',
+    author_email='ebattat@redhat.com, athiruma@redhat.com',
     url='',
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 
     zip_safe=False,
