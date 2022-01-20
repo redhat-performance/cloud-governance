70b7e09633cc84415389a1e622fbcb7e3544581b
AARAVETI THIRUMALESH <athiruma@athiruma.remote.csb>
2022-01-20 18:46:47 +0530

    Added the python3.8 and python3.9 to PR.yml

--- tests/cloud_governance/common/es/test_es_operations.py
+++ tests/cloud_governance/common/es/test_es_operations.py
@@ -1,6 +1,5 @@
 from cloud_governance.common.es.es_operations import ESOperations
 
-
 def test_get_s3_latest_policy_file():
     es_operations = ESOperations(es_host='localhost', es_port=9200, region='us-east-2', bucket='redhat-cloud-governance', logs_bucket_key='logs')
     assert es_operations._ESOperations__get_s3_latest_policy_file(policy='ec2-idle')
