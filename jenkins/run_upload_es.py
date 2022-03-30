66b6b96d723a2585866a3a8113aa39eda90b0193
AARAVETI THIRUMALESH <athiruma@athiruma.remote.csb>
2022-03-30 21:22:18 +0530

    Updated the run_polices list error & added emial to Jenkins failed scripts

--- jenkins/run_upload_es.py
+++ jenkins/run_upload_es.py
@@ -30,7 +30,7 @@ def get_custodian_policies(type: str = None):
     return custodian_policies
 
 
-regions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'eu-central-1']
+regions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'eu-central-1', 'ap-south-1', 'eu-north-1', 'ap-northeast-1', 'ap-southeast-1', 'ap-southeast-2', 'eu-south-1', 'eu-west-3', 'sa-east-1']
 
 print("Upload data to ElasticSearch - ec2 index")
 policies = get_custodian_policies(type='ec2')
