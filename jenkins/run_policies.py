8fe468603e224a10873fd52ae3bed4e2a77d6102
AARAVETI THIRUMALESH <athiruma@athiruma.remote.csb>
2022-03-31 17:14:45 +0530

    Removed the us-south-1 region in run_policies & run_upload_es

--- jenkins/run_policies.py
+++ jenkins/run_policies.py
@@ -33,7 +33,7 @@ def get_custodian_policies(type: str = None):
 
 
 print('Run all policies pre active region')
-regions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'eu-central-1', 'ap-south-1', 'eu-north-1', 'ap-northeast-1', 'ap-southeast-1', 'ap-southeast-2', 'eu-south-1', 'eu-west-3', 'sa-east-1']
+regions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'eu-central-1', 'ap-south-1', 'eu-north-1', 'ap-northeast-1', 'ap-southeast-1', 'ap-southeast-2', 'eu-west-3', 'sa-east-1']
 policies = get_custodian_policies()
 
 for region in regions:
