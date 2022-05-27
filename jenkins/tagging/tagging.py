990ab8251fafeac6f525ac7c017b658e6ba37482
AARAVETI THIRUMALESH <athiruma@athiruma.remote.csb>
2022-05-27 17:27:16 +0530

    Added the clean workspace

--- jenkins/tagging/tagging.py
+++ jenkins/tagging/tagging.py
@@ -1,3 +1,4 @@
+
 import os
 
 AWS_ACCESS_KEY_ID_PERF = os.environ['AWS_ACCESS_KEY_ID_PERF']
@@ -17,8 +18,7 @@ LOGS = os.environ.get('LOGS', 'logs')
 mandatory_tags = "{'Budget': 'PERF-DEPT'}"
 
 print('Run all policies pre active region')
-regions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'eu-central-1', 'ap-south-1', 'eu-north-1',
-           'ap-northeast-1', 'ap-southeast-1', 'ap-southeast-2', 'eu-west-3', 'sa-east-1']
+regions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'eu-central-1', 'ap-south-1', 'eu-north-1', 'ap-northeast-1', 'ap-southeast-1', 'ap-southeast-2', 'eu-west-3', 'sa-east-1']
 
 for region in regions:
-    os.system(f'sudo podman run --rm --name cloud-governance -e account="perf" -e policy=tag_resources -e AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID_DELETE_PERF} -e AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY_DELETE_PERF} -e AWS_DEFAULT_REGION={region} -e tag_operation=update -e mandatory_tags={mandatory_tags} -e log_level=INFO -v /etc/localtime:/etc/localtime quay.io/ebattat/cloud-governance')
+    os.system(f"sudo podman run --rm --name cloud-governance-tagging -e account='perf' -e policy=tag_resources -e AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID_DELETE_PERF} -e AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY_DELETE_PERF} -e AWS_DEFAULT_REGION={region} -e tag_operation=update -e mandatory_tags={mandatory_tags} -e log_level=INFO -v /etc/localtime:/etc/localtime quay.io/ebattat/cloud-governance:latest")
