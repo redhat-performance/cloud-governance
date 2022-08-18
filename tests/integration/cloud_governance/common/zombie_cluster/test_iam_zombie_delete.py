0226b290bd66c0cd73e7421bdcf4dd5e9ffa2212
Thirumalesh Aaraveti <97395760+athiruma@users.noreply.github.com>
2022-08-18 15:20:48 +0530

    Added user terraform test and added s3:// to build yaml (#448)
    Co-authored-by: AARAVETI THIRUMALESH <athiruma@athiruma.remote.csb>

--- tests/integration/cloud_governance/common/zombie_cluster/test_iam_zombie_delete.py
+++ tests/integration/cloud_governance/common/zombie_cluster/test_iam_zombie_delete.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 from datetime import datetime
 
@@ -7,40 +8,28 @@ from cloud_governance.common.aws.utils.utils import Utils
 from cloud_governance.policy.zombie_cluster_resource import ZombieClusterResources
 
 
+USER_NAME = os.environ.get('USER_NAME', '')
+iam_resource = boto3.client('iam')
+
+
 def test_iam_zombie_user_create_and_delete():
     """
     This method checks weather the zombie users exists or not
     :return:
     """
-    short_random_id = uuid.uuid1()
-    time_ms = str(datetime.utcnow().strftime('%f'))
-    USER_NAME = f'integration-ocp-{short_random_id}-{time_ms}'
-    iam_resource = boto3.client('iam')
-    tags = [
-        {'Key': f'kubernetes.io/cluster/{USER_NAME}', 'Value': 'Owned'},
-        {'Key': 'Owner', 'Value': 'integration'}
-    ]
-    try:
-        iam_resource.create_user(UserName=USER_NAME, Tags=tags)
-        zombie_cluster_resources = ZombieClusterResources(cluster_prefix='kubernetes.io/cluster/', delete=False,
-                                                          cluster_tag=f'kubernetes.io/cluster/{USER_NAME}',
-                                                          resource_name='zombie_cluster_user')
-        assert len(zombie_cluster_resources.zombie_cluster_user()) >= 1
-        zombie_cluster_resources = ZombieClusterResources(cluster_prefix='kubernetes.io/cluster/', delete=True,
-                                                          cluster_tag=f'kubernetes.io/cluster/{USER_NAME}',
-                                                          resource_name='zombie_cluster_user')
-        zombie_cluster_resources.zombie_cluster_user()
-        iam_users = Utils().get_details_resource_list(func_name=iam_resource.list_users, input_tag='Users',
-                                                      check_tag='Marker')
-        find = False
-        for user in iam_users:
-            if user['UserName'] == USER_NAME:
-                find = True
-                break
-        assert not find
-    except Exception as err:
-        iam_users = Utils().get_details_resource_list(func_name=iam_resource.list_users, input_tag='Users', check_tag='Marker')
-        for user in iam_users:
-            if user['UserName'] == USER_NAME:
-                iam_resource.delete_user(UserName=USER_NAME)
-        raise Exception('Failed to delete User')
+    zombie_cluster_resources = ZombieClusterResources(cluster_prefix='kubernetes.io/cluster/', delete=False,
+                                                      cluster_tag=f'kubernetes.io/cluster/{USER_NAME}',
+                                                      resource_name='zombie_cluster_user')
+    assert len(zombie_cluster_resources.zombie_cluster_user()) >= 1
+    zombie_cluster_resources = ZombieClusterResources(cluster_prefix='kubernetes.io/cluster/', delete=True,
+                                                      cluster_tag=f'kubernetes.io/cluster/{USER_NAME}',
+                                                      resource_name='zombie_cluster_user')
+    zombie_cluster_resources.zombie_cluster_user()
+    iam_users = Utils().get_details_resource_list(func_name=iam_resource.list_users, input_tag='Users',
+                                                  check_tag='Marker')
+    find = False
+    for user in iam_users:
+        if user['UserName'] == USER_NAME:
+            find = True
+            break
+    assert not find
