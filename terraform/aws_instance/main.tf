d2db2cf6d5e746602b781c57792aa253a1eede4f
Thirumalesh Aaraveti <97395760+athiruma@users.noreply.github.com>
2022-08-18 16:26:02 +0530

    Revert "Added user terraform test and added s3:// to build yaml (#448)"
    This reverts commit 0226b290bd66c0cd73e7421bdcf4dd5e9ffa2212.

--- terraform/aws_instance/main.tf
+++ terraform/aws_instance/main.tf
@@ -1,18 +1,3 @@
-
-resource "random_id" "uuid" {
-  byte_length = 8
-}
-
-resource "aws_iam_user" "user"{
-  name = "CloudGovernanceTest${random_id.uuid.hex}"
-
-  tags = {
-    "kubernetes.io/cluster/CloudGovernanceTest${random_id.uuid.hex}" = "owned"
-    Purpose = "integration-test"
-  }
-}
-
-
 resource "aws_instance" "main" {
   for_each = var.instance_details
   instance_type = each.value.instance_type
