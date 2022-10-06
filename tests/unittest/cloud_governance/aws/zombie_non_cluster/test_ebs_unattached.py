4019a5f65823e4a0613a0f707c9d8d0d9447912a
AARAVETI THIRUMALESH <athiruma@athiruma.remote.csb>
2022-10-06 12:44:48 +0530

    Added mail, skip for ebs_unattached

--- tests/unittest/cloud_governance/aws/zombie_non_cluster/test_ebs_unattached.py
+++ tests/unittest/cloud_governance/aws/zombie_non_cluster/test_ebs_unattached.py
@@ -39,8 +39,7 @@ def test_ebs_unattached_skip_deletion():
     os.environ['policy'] = 'ebs_unattached'
     ec2_client = boto3.client('ec2', region_name=os.environ.get('AWS_DEFAULT_REGION'))
     tags = [{'Key': 'policy', 'Value': 'skip'}]
-    ec2_client.create_volume(AvailabilityZone='us-east-2a', Size=10,
-                             TagSpecifications=[{'ResourceType': 'volume', 'Tags': tags}])
+    ec2_client.create_volume(AvailabilityZone='us-east-2a', Size=10, TagSpecifications=[{'ResourceType': 'volume', 'Tags': tags}])
     zombie_elastic_ips = NonClusterZombiePolicy()
     zombie_elastic_ips.DAYS_TO_TRIGGER_RESOURCE_MAIL = -1
     zombie_elastic_ips._check_resource_and_delete(resource_name='Ebs Volume',
@@ -70,3 +69,4 @@ def test_skip_live_cluster_volumes():
     ebs_unattached = EbsUnattached()
     ebs_unattached.run()
     assert 2 == len(ec2_client.describe_volumes()['Volumes'])
+
