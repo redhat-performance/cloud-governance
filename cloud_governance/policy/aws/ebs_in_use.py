cd3aadee8ebc048630ae28a0b5db8375fc105c07
Thirumalesh Aaraveti <97395760+athiruma@users.noreply.github.com>
2022-11-02 12:26:38 +0530

    Removed c7n polices (#511)
    Co-authored-by: AARAVETI THIRUMALESH <athiruma@athiruma.remote.csb>

---
+++ cloud_governance/policy/aws/ebs_in_use.py
@@ -0,0 +1,21 @@
+import json
+
+from cloud_governance.aws.zombie_non_cluster.run_zombie_non_cluster_policies import NonClusterZombiePolicy
+
+
+class EbsInUse(NonClusterZombiePolicy):
+
+    def __init__(self):
+        super(EbsInUse, self).__init__()
+
+    def run(self):
+        """
+        This method list all in-use ebs volumes
+        @return:
+        """
+        in_use_volumes = []
+        volumes = self._ec2_operations.get_volumes()
+        for volume in volumes:
+            if volume.get('State') == 'in-use':
+                in_use_volumes.append(volume)
+        return in_use_volumes
