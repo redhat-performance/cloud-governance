6b0b8e3396c8b1c169462b7b72ae920a1384cf5f
AARAVETI THIRUMALESH <athiruma@athiruma.remote.csb>
2022-12-16 11:51:28 +0530

    Added the test for aws cost explorer

--- docs/source/configuration.md
+++ docs/source/configuration.md
@@ -3,8 +3,8 @@
 ### AWS Configuration
 
 #### Create a user and a bucket
-* Create user with IAM [iam](iam)
-* Create a logs bucket [create_bucket.sh](iam/create_bucket.sh)
+* Create user with IAM [iam](iam/clouds)
+* Create a logs bucket [create_bucket.sh](iam/cloud/aws/create_bucket.sh)
 
 ### IBM Configuration
 * Create classic infrastructure API key
---
+++ tests/integration/cloud_governance/common/clouds/aws/cost_explorer/test_cost_explorer_operations.py
@@ -0,0 +1,23 @@
+import datetime
+
+from cloud_governance.common.clouds.aws.cost_explorer.cost_explorer_operations import CostExplorerOperations
+
+
+def test_get_cost_and_usage_from_aws():
+    cost_explorer_operations = CostExplorerOperations()
+    end_date = datetime.datetime.now()
+    start_date = end_date - datetime.timedelta(1)
+    start_date = str(start_date.strftime('%Y-%m-%d'))
+    end_date = str(end_date.strftime('%Y-%m-%d'))
+    aws_usage = cost_explorer_operations.get_cost_and_usage_from_aws(start_date=start_date, end_date=end_date)
+    assert aws_usage
+
+
+def test_get_cost_forecast():
+    cost_explorer_operations = CostExplorerOperations()
+    start_date = datetime.datetime.now()
+    end_date = start_date + datetime.timedelta(1)
+    start_date = str(start_date.strftime('%Y-%m-%d'))
+    end_date = str(end_date.strftime('%Y-%m-%d'))
+    aws_forecast = cost_explorer_operations.get_cost_forecast(start_date=start_date, end_date=end_date, granularity='MONTHLY', cost_metric='UNBLENDED_COST')
+    assert aws_forecast
