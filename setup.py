779369804c1d2de302c09dda7b0990de9f744fc4
Thirumalesh Aaraveti <97395760+athiruma@users.noreply.github.com>
2022-12-17 23:05:53 +0530

    Fixed the setup.py file (#540)
    Co-authored-by: AARAVETI THIRUMALESH <athiruma@athiruma.remote.csb>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 from setuptools import setup, find_packages
 
 
-__version__ = '1.1.48'
+__version__ = '1.1.49'
 
 
 here = path.abspath(path.dirname(__file__))
@@ -42,28 +42,28 @@ setup(
 
     install_requires=[
         'attrs==21.4.0',  # readthedocs
+        'azure-identity==1.12.0',  # azure identity
+        'azure-mgmt-costmanagement==3.0.0',  # azure cost management
+        'azure-mgmt-subscription==3.1.1',  # azure subscriptions
         'botocore==1.29.1',  # required by c7n 0.9.14
         'boto3==1.26.1',  # required by c7n 0.9.14
         'elasticsearch==7.11.0',  # depend on elasticsearch server
         'elasticsearch-dsl==7.4.0',
+        'google-api-python-client==2.57.0',  # google drive
+        'google-auth-httplib2==0.1.0',  # google drive
+        'google-auth-oauthlib==0.5.2',  # google drive
+        'ibm_platform_services==0.27.0',  # IBM Usage reports
         'myst-parser==0.17.0',  # readthedocs
         'pandas',  # latest: aggregate ec2/ebs cluster data
         'PyGitHub==1.55',  # gitleaks
+        'python-ldap==3.4.2',  # prerequisite: sudo dnf install -y python39-devel openldap-devel gcc
         'requests==2.27.1',  # rest api & lambda
+        'retry==0.9.2',
+        'SoftLayer==6.0.0',  # IBM SoftLayer
         'sphinx==4.5.0',  # readthedocs
         'sphinx-rtd-theme==1.0.0',  # readthedocs
         'typing==3.7.4.3',
         'typeguard==2.13.3',  # checking types
-        'google-api-python-client==2.57.0',  # google drive
-        'google-auth-httplib2==0.1.0',  # google drive
-        'google-auth-oauthlib==0.5.2',  # google drive
-        'python-ldap==3.4.2',   # prerequisite: sudo dnf install -y python39-devel openldap-devel gcc
-        'SoftLayer==6.0.0',  # IBM SoftLayer
-        'retry==0.9.2',
-        'ibm_platform_services==0.27.0'  # IBM Usage reports
-        'azure-identity==1.12.0'  # azure identity
-        'azure-mgmt-costmanagement==3.0.0'  # azure cost management
-        'azure-mgmt-subscription==3.1.1'  # azure subscriptions
     ],
 
     setup_requires=['pytest', 'pytest-runner', 'wheel', 'coverage'],
