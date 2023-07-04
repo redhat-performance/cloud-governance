8a30c04699ebf3f8f63853e176ba9fcce79a4c28
Thirumalesh Aaraveti <97395760+athiruma@users.noreply.github.com>
2023-07-04 18:02:18 +0530

    Missed the start, end date time for the cost usage function (#628)

--- tests/integration/cloud_governance/common/clouds/azure/cost_management/test_cost_management_operations.py
+++ tests/integration/cloud_governance/common/clouds/azure/cost_management/test_cost_management_operations.py
@@ -1,5 +1,7 @@
 import datetime
 
+import pytest
+
 from cloud_governance.common.clouds.azure.cost_management.cost_management_operations import CostManagementOperations
 
 
@@ -9,7 +11,13 @@ def test_get_usage():
     @return:
     """
     cost_management_operations = CostManagementOperations()
-    cost_usage_data = cost_management_operations.get_usage(scope=cost_management_operations.azure_operations.scope)
+    end_date = datetime.datetime.utcnow() - datetime.timedelta(days=2)
+    start_date = end_date - datetime.timedelta(days=1)
+    granularity = 'Daily'
+    cost_usage_data = cost_management_operations.get_usage(scope=cost_management_operations.azure_operations.scope,
+                                                           start_date=start_date, end_date=end_date,
+                                                           granularity=granularity
+                                                           )
     assert cost_usage_data
 
 
