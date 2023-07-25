017ccd41c014ea45694d5387c6c53339b6ca0431
Thirumalesh Aaraveti <athiruma@redhat.com>
2023-07-25 18:58:59 +0530

    Added the account name

--- cloud_governance/policy/policy_operations/aws/zombie_non_cluster/zombie_non_cluster_polices.py
+++ cloud_governance/policy/policy_operations/aws/zombie_non_cluster/zombie_non_cluster_polices.py
@@ -32,6 +32,7 @@ class ZombieNonClusterPolicies(NonClusterZombiePolicy):
                         if policy_result:
                             for policy_dict in policy_result:
                                 policy_dict['region_name'] = self._region
+                                policy_dict['account'] = self._account
                                 self._es_operations.upload_to_elasticsearch(data=policy_dict, index=self._es_index)
                             logger.info(f'Uploaded the pilicy results to elasticsearch index: {self._es_index}')
                         else:
