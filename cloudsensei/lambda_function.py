d50e72490c702d740668a369195022822251fea6
Thirumalesh Aaraveti <athiruma@redhat.com>
2023-10-09 17:29:46 +0530

    Added the instance_type to report

--- cloudsensei/lambda_function.py
+++ cloudsensei/lambda_function.py
@@ -77,7 +77,8 @@ class EC2Operations:
                             long_running_instances_by_user.setdefault(user.lower(), {}).setdefault(region_name, []).append(
                                 {'InstanceId': resource.get('InstanceId'),
                                  'Name': name, 'LaunchDate': str(launch_time),
-                                 'RunningDays': f"{days} days", 'State': resource.get('State', {}).get('Name')})
+                                 'RunningDays': f"{days} days", 'State': resource.get('State', {}).get('Name'),
+                                 'InstanceType': resource.get('InstanceType')})
         return long_running_instances_by_user
 
     def get_account_alias_name(self):
@@ -97,7 +98,7 @@ class EC2Operations:
         """
 
         divider = {"type": "divider"}
-        keys_list = ['User', 'Region', 'Name', 'InstanceId', 'LaunchDate', 'RunningDays']
+        keys_list = ['User', 'Region', 'Name', 'InstanceType', 'InstanceId', 'LaunchDate', 'RunningDays']
         rows = []
         for user, region_list in resources_list.items():
             for region_name, resources_list in region_list.items():
@@ -121,7 +122,7 @@ class EC2Operations:
         :param resources_list:
         :return:
         """
-        keys_list = ['User', 'Region', 'Name', 'InstanceId', 'LaunchDate', 'State',  'RunningDays']
+        keys_list = ['User', 'Region', 'Name', 'InstanceType', 'InstanceId', 'LaunchDate', 'State',  'RunningDays']
         with open('email_template.j2') as template:
             template = Template(template.read())
             body = template.render({'resources_list': resources_list, 'keys_list': keys_list})
