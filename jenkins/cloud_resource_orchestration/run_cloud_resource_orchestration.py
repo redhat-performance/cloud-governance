81f5a9112a6420a38aac4003c6152972a49e25a5
Thirumalesh Aaraveti <97395760+athiruma@users.noreply.github.com>
2023-11-23 19:38:30 +0530

    Activated the azure cro (#691)

--- jenkins/cloud_resource_orchestration/run_cloud_resource_orchestration.py
+++ jenkins/cloud_resource_orchestration/run_cloud_resource_orchestration.py
@@ -1,4 +1,3 @@
-
 import os
 
 AWS_ACCESS_KEY_ID_DELETE_PERF = os.environ['AWS_ACCESS_KEY_ID_DELETE_PERF']
@@ -29,6 +28,15 @@ ATHENA_TABLE_NAME = os.environ['ATHENA_TABLE_NAME']
 
 es_index = CLOUD_RESOURCE_ORCHESTRATION_INDEX
 
+common_env_vars = {
+    'es_host': ES_HOST, 'es_port': ES_PORT, 'CRO_ES_INDEX': CRO_ES_INDEX, 'log_level': 'INFO',
+    'LDAP_HOST_NAME': LDAP_HOST_NAME,
+    'JIRA_QUEUE': JIRA_QUEUE, 'JIRA_TOKEN': JIRA_TOKEN, 'JIRA_USERNAME': JIRA_USERNAME, 'JIRA_URL': JIRA_URL,
+    'CRO_COST_OVER_USAGE': CRO_COST_OVER_USAGE, 'CRO_PORTAL': CRO_PORTAL, 'CRO_DEFAULT_ADMINS': CRO_DEFAULT_ADMINS,
+    'CRO_REPLACED_USERNAMES': CRO_REPLACED_USERNAMES,
+    'CE_PAYER_INDEX': 'cloud-governance-clouds-billing-reports', 'RUN_ACTIVE_REGIONS': True,
+}
+
 input_vars_to_container = [{'account': 'perf-dept', 'AWS_ACCESS_KEY_ID': AWS_ACCESS_KEY_ID_DELETE_PERF,
                             'AWS_SECRET_ACCESS_KEY': AWS_SECRET_ACCESS_KEY_DELETE_PERF, 'PUBLIC_CLOUD_NAME': 'AWS'},
                            {'account': 'perf-scale', 'AWS_ACCESS_KEY_ID': AWS_ACCESS_KEY_ID_DELETE_PERF_SCALE,
@@ -38,11 +46,16 @@ input_vars_to_container = [{'account': 'perf-dept', 'AWS_ACCESS_KEY_ID': AWS_ACC
 
 os.system('echo Run CloudResourceOrchestration in pre active region')
 
-common_input_vars = {'es_host': ES_HOST, 'es_port': ES_PORT, 'CRO_ES_INDEX': CRO_ES_INDEX, 'log_level': 'INFO', 'LDAP_HOST_NAME': LDAP_HOST_NAME,
-                     'JIRA_QUEUE': JIRA_QUEUE, 'JIRA_TOKEN': JIRA_TOKEN, 'JIRA_USERNAME': JIRA_USERNAME, 'JIRA_URL': JIRA_URL,
-                     'CRO_COST_OVER_USAGE': CRO_COST_OVER_USAGE, 'CRO_PORTAL': CRO_PORTAL, 'CRO_DEFAULT_ADMINS': CRO_DEFAULT_ADMINS, 'CRO_REPLACED_USERNAMES': CRO_REPLACED_USERNAMES,
-                     'CE_PAYER_INDEX': 'cloud-governance-clouds-billing-reports', 'RUN_ACTIVE_REGIONS': True, 'AWS_DEFAULT_REGION': 'us-east-1', 'AWS_MAX_ATTEMPTS': 5, 'AWS_RETRY_MODE': 'standard',
-                     'ATHENA_ACCOUNT_ACCESS_KEY': AWS_ACCESS_KEY_ID_ATHIRUMA_BOT, 'ATHENA_ACCOUNT_SECRET_KEY': AWS_SECRET_ACCESS_KEY_ATHIRUMA_BOT,
+common_input_vars = {'es_host': ES_HOST, 'es_port': ES_PORT, 'CRO_ES_INDEX': CRO_ES_INDEX, 'log_level': 'INFO',
+                     'LDAP_HOST_NAME': LDAP_HOST_NAME,
+                     'JIRA_QUEUE': JIRA_QUEUE, 'JIRA_TOKEN': JIRA_TOKEN, 'JIRA_USERNAME': JIRA_USERNAME,
+                     'JIRA_URL': JIRA_URL,
+                     'CRO_COST_OVER_USAGE': CRO_COST_OVER_USAGE, 'CRO_PORTAL': CRO_PORTAL,
+                     'CRO_DEFAULT_ADMINS': CRO_DEFAULT_ADMINS, 'CRO_REPLACED_USERNAMES': CRO_REPLACED_USERNAMES,
+                     'CE_PAYER_INDEX': 'cloud-governance-clouds-billing-reports', 'RUN_ACTIVE_REGIONS': True,
+                     'AWS_DEFAULT_REGION': 'us-east-1', 'AWS_MAX_ATTEMPTS': 5, 'AWS_RETRY_MODE': 'standard',
+                     'ATHENA_ACCOUNT_ACCESS_KEY': AWS_ACCESS_KEY_ID_ATHIRUMA_BOT,
+                     'ATHENA_ACCOUNT_SECRET_KEY': AWS_SECRET_ACCESS_KEY_ATHIRUMA_BOT,
                      'ATHENA_DATABASE_NAME': ATHENA_DATABASE_NAME, 'ATHENA_TABLE_NAME': ATHENA_TABLE_NAME,
                      'S3_RESULTS_PATH': S3_RESULTS_PATH
                      }
@@ -56,3 +69,23 @@ for input_vars in input_vars_to_container:
     os.system(f"""echo Running on Account {input_vars.get("account").upper()}""")
     envs = list(map(combine_vars, input_vars.items()))
     os.system(f"""podman run --net="host" --rm --name  cloud_resource_orchestration -e CLOUD_RESOURCE_ORCHESTRATION="True" -e EMAIL_ALERT="True" -e {' -e '.join(envs)} -e {' -e '.join(common_envs)} quay.io/ebattat/cloud-governance:latest""")
+
+
+AZURE_ACCOUNT_ID = os.environ['AZURE_ACCOUNT_ID']
+AZURE_CLIENT_SECRET = os.environ['AZURE_CLIENT_SECRET']
+AZURE_TENANT_ID = os.environ['AZURE_TENANT_ID']
+AZURE_CLIENT_ID = os.environ['AZURE_CLIENT_ID']
+AZURE_SUBSCRIPTION_ID = os.environ['AZURE_SUBSCRIPTION_ID']
+
+os.system("Running the azure cro")
+azure_cro_env = {
+    'AZURE_ACCOUNT_ID': AZURE_ACCOUNT_ID, 'AZURE_CLIENT_ID': AZURE_CLIENT_ID,
+    'AZURE_TENANT_ID': AZURE_TENANT_ID, 'AZURE_CLIENT_SECRET': AZURE_CLIENT_SECRET,
+    'AZURE_SUBSCRIPTION_ID': AZURE_SUBSCRIPTION_ID,
+    'account': 'PERF&SCALE', 'PUBLIC_CLOUD_NAME': 'AZURE', 'CLOUD_RESOURCE_ORCHESTRATION': True
+}
+azure_cro_env.update(common_env_vars)
+
+azure_cro = """ podman run --net="host" --rm --name cloud_resource_orchestration """
+azure_cro += f" -e {' -e '.join(azure_cro_env)}  quay.io/ebattat/cloud-governance:latest"
+os.system(azure_cro)
