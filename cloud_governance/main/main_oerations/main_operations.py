b612a2486b0bb7d296c450cfc663e54041725d61
Thirumalesh Aaraveti <97395760+athiruma@users.noreply.github.com>
2024-02-20 15:18:44 +0530

    Moved the policy to cleanup (#732)

--- cloud_governance/main/main_oerations/main_operations.py
+++ cloud_governance/main/main_oerations/main_operations.py
@@ -40,6 +40,9 @@ class MainOperations:
             # @Todo support for all the aws policies, currently supports ec2_run as urgent requirement
             if self._policy in policies and self._policy in ["instance_run", "unattached_volume", "cluster_run",
                                                              "ip_unattached"]:
-                policy_runner.run(source=policy_type)
+                source = policy_type
+                if Utils.equal_ignore_case(policy_type, self._public_cloud_name):
+                    source = ''
+                policy_runner.run(source=source)
                 return True
         return False
