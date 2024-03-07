baf05ac25d57b9ae9ae6ea1c4f808d53ee7c24b4
ebattat <ebattat@redhat.com>
2024-03-07 13:46:09 +0000

    bump version to exist version v1.1.217

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from codecs import open
 from os import path
 from setuptools import setup, find_packages
 
-__version__ = '1.1.217'
+__version__ = '1.1.218'
 
 here = path.abspath(path.dirname(__file__))
 
