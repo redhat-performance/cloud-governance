f5b5bd7d5ca167b4edc6f0ea587ac3ca74410952
Thirumalesh Aaraveti <athiruma@redhat.com>
2024-09-04 18:52:14 +0530

    Add the subnet_id to terraform

--- terraform/aws_instance/main.tf
+++ terraform/aws_instance/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "main" {
-  for_each = var.instance_details
+  for_each      = var.instance_details
   instance_type = each.value.instance_type
-  ami = each.value.image_id
-  tags = each.value.tags
+  ami           = each.value.image_id
+  tags          = each.value.tags
+  subnet_id     = each.value.subnet_id
 }
