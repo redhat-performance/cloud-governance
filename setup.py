000d35203832a46beae191ec62db2e19ef2ecec4
Thirumalesh Aaraveti <97395760+athiruma@users.noreply.github.com>
2024-09-24 17:16:51 +0530

    Remove unsupported python version (#846)

--- setup.py
+++ setup.py
@@ -25,8 +25,6 @@ setup(
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
