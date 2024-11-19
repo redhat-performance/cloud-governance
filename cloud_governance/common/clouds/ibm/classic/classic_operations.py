884d03dd26a1b7bf58744c114d4dfdfa03a9d06e
Thirumalesh Aaraveti <athiruma@redhat.com>
2024-11-19 17:16:59 +0530

    Added the tags to classic bm/vm

--- cloud_governance/common/clouds/ibm/classic/classic_operations.py
+++ cloud_governance/common/clouds/ibm/classic/classic_operations.py
@@ -2,6 +2,7 @@ from retry import retry
 from typeguard import typechecked
 
 from cloud_governance.common.clouds.ibm.account.ibm_account import IBMAccount
+from cloud_governance.common.jira.jira import logger
 from cloud_governance.common.logger.logger_time_stamp import logger_time_stamp
 
 
@@ -97,3 +98,55 @@ class ClassicOperations:
         """
         tags = self._sl_client.call('SoftLayer_Virtual_Guest', 'getTagReferences', id=vm_id)
         return self.__filter_tag_names(tag_references=tags)
+
+    def update_baremetal_tags(self, tags: list, hardware_id: str):
+        """
+        This method updates the hardware tags
+        :param tags:
+        :param hardware_id:
+        :return:
+        """
+        tag_names = []
+        for tag in tags:
+            key, value = tag.split(':')
+            tag_names.append(f"{key.strip()}:{value.strip()}")
+        error = ''
+        try:
+            response = self._sl_client.call('SoftLayer_Hardware_Server',
+                                            'setTags', ','.join(tag_names), id=hardware_id)
+            if response:
+                logger.info(f'Tags are added to the hardware: {hardware_id}  : {tag_names}')
+            else:
+                logger.error(
+                    f'Tags are not added to the hardware: {hardware_id} - , something might fail')
+        except Exception as err:
+            error = str(err)
+            response = False
+            logger.error(f'{err}')
+        return response, error
+
+    def update_virtual_machine_tags(self, tags: list, virtual_machine_id: str):
+        """
+        This method updates the virtual machine tags
+        :param tags:
+        :param virtual_machine_id:
+        :return:
+        """
+        tag_names = []
+        for tag in tags:
+            key, value = tag.split(':')
+            tag_names.append(f"{key.strip()}:{value.strip()}")
+        error = ''
+        try:
+            response = self._sl_client.call('SoftLayer_Virtual_Guest',
+                                            'setTags', ','.join(tag_names), id=virtual_machine_id)
+            if response:
+                logger.info(f'Tags are added to the VirtualMachine: {virtual_machine_id}  : {tags}')
+            else:
+                logger.error(
+                    f'Tags are not added to the VirtualMachine: {virtual_machine_id} - , something might fail')
+        except Exception as err:
+            error = str(err)
+            response = False
+            logger.error(f'{err}')
+        return response, error
