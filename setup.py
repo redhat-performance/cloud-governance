08f806ddd5a0c2370e8670794980b08a229076a0
Thirumalesh Aaraveti <athiruma@redhat.com>
2024-11-25 16:34:41 +0530

    Fixed the pypi version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from codecs import open
 from os import path
 from setuptools import setup, find_packages
 
-__version__ = '1.1.333'
+__version__ = '1.1.334'
 
 here = path.abspath(path.dirname(__file__))
 
