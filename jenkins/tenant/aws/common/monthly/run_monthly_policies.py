fa8ea2d1c5fa6d00315051af9e325c5515ccc75e
Pragya Chaudhary <pragchau@redhat.com>
2026-01-15 14:00:23 +0530

    Add policy to monthly jenkinsfile eco eng

--- jenkins/tenant/aws/common/monthly/run_monthly_policies.py
+++ jenkins/tenant/aws/common/monthly/run_monthly_policies.py
@@ -4,8 +4,9 @@ account_name = os.environ['account_name']
 ES_HOST = os.environ['ES_HOST']
 ES_PORT = os.environ['ES_PORT']
 
-QUAY_CLOUD_GOVERNANCE_REPOSITORY = os.environ.get('QUAY_CLOUD_GOVERNANCE_REPOSITORY',
-                                                  'quay.io/cloud-governance/cloud-governance')
+# QUAY_CLOUD_GOVERNANCE_REPOSITORY = os.environ.get('QUAY_CLOUD_GOVERNANCE_REPOSITORY',
+#                                                   'quay.io/cloud-governance/cloud-governance')
+QUAY_CLOUD_GOVERNANCE_REPOSITORY = 'quay.io/rh-ee-pragchau/cloud-governance:latest'
 
 os.system(f"""echo "Running yearly savings report for account {account_name}" """)
 os.system(f"""podman run --rm --net="host" --name cloud-governance -e policy="yearly_savings_report" \
