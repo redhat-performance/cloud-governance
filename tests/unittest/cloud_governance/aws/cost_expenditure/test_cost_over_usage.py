a795bf029d0d9bf2dea800f1044bede6eb146c7b
Pragya Chaudhary <pragchau@redhat.com>
2026-01-22 17:27:39 +0530

    fix ut issue

--- tests/unittest/cloud_governance/aws/cost_expenditure/test_cost_over_usage.py
+++ tests/unittest/cloud_governance/aws/cost_expenditure/test_cost_over_usage.py
@@ -10,10 +10,16 @@ def test_aggregate_user_sum():
 
 
 def test_aggregate_user_sum_instances():
-    test_df = [{'User': 'test1', 'Cost': 9, 'Instances': [{'InstanceId': 1234}]},
-               {'User': 'test1', 'Cost': 9, 'Instances': [{'InstanceId': 1234}]},
-               {'User': 'test2', 'Cost': 14}, {'User': 'test2', 'Cost': 19}]
-    expected_df = [{'User': 'test1', 'Cost': 18, 'Instances': [{'InstanceId': 1234}]}, {'User': 'test2', 'Cost': 33, 'Instances': []}]
+    test_df = [
+        {'User': 'test1', 'Cost': 9, 'Instances': [{'InstanceId': 1234}]},
+        {'User': 'test1', 'Cost': 9, 'Instances': [{'InstanceId': 1234}]},
+        {'User': 'test2', 'Cost': 14, 'Instances': []},
+        {'User': 'test2', 'Cost': 19, 'Instances': []}
+    ]
+    expected_df = [
+        {'User': 'test1', 'Cost': 18, 'Instances': [{'InstanceId': 1234}]},
+        {'User': 'test2', 'Cost': 33, 'Instances': []}
+    ]
     cost_over_usage = CostOverUsage()
     actual_df = cost_over_usage.aggregate_user_sum(test_df)
     assert actual_df == expected_df
